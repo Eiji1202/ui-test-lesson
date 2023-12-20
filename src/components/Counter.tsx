43edf3359604122b2d938e8c91c461f31b380118
Eiji1202 <tsuyoku1202@gmail.com>
2023-12-21 07:44:11 +0900

    Button label changed

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -22,8 +22,8 @@ const Counter: React.FC<CounterProps> = ({ initialCount = 0 }) => {
   return (
     <div>
       <h2>Count: {count}</h2>
-      <button onClick={decrement}>ー</button>
-      <button onClick={increment}>＋</button>
+      <button onClick={decrement}>マイナス</button>
+      <button onClick={increment}>プラス</button>
     </div>
   );
 };
